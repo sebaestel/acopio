2834cede4c8d7cc210b902098d3ddf28ad827d82
Sebastian Durán <yo@sebaduran.com>
2017-01-28 01:19:37 -0300

    Add indications and integration with google street view images

--- build/app/homeController.js
+++ build/app/homeController.js
@@ -1,14 +1,23 @@
 app.controller('homeController', function($scope, NgMap, $http) {
 	$scope.title =  "Map";
     $scope.showPlace = false;
+	$scope.mapLoader = true;
 	$scope.placeChanged = function() {
 		$scope.searchPlace = this.getPlace();
 		$scope.map.setCenter($scope.searchPlace.geometry.location);
 	}
+	navigator.geolocation.getCurrentPosition(function(position) {
+		$scope.currentLocation = position.coords.latitude + "," + position.coords.longitude;
+		$scope.mapLoader = false;
+	}, function(e) {
+		$scope.currentLocation = "-33.4556033, -70.6450233";
+		$scope.mapLoader = false;
+	});
 
     $scope.placeInfo = function (event, info) {
     	$scope.place = info;
     	$scope.showPlace = true;
+    	$scope.tripTo = info.pos.lat + "," + info.pos.lon;
     }
 
 	$http
